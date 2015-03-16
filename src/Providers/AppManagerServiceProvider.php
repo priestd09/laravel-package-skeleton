711ff7e7091b68023c582ead0281f42ba419b441
Marco Bunge <hello@rubymatrix.de>
2015-03-16 09:34:00 +0100

    Update AppManagerServiceProvider.php

--- src/Providers/AppManagerServiceProvider.php
+++ src/Providers/AppManagerServiceProvider.php
@@ -1,34 +1,18 @@
 <?php
 /*
-* This file is part of the Monolog package.
 *
 * (c) Marco Bunge <marco_bunge@web.de>
 *
 * For the full copyright and license information, please view the LICENSE.txt
 * file that was distributed with this source code.
-*
-* Date: 24.02.2015
-* Time: 13:45
+* 
 */
-namespace DigitalBaker\AppManager\Providers;
+namespace VendorName\PackageName\Providers;
 
-use DigitalBaker\AppManager\Managers\SourceManager;
 use Illuminate\Support\ServiceProvider;
 
-class AppManagerServiceProvider extends ServiceProvider
+class PackageServiceProvider extends ServiceProvider
 {
-
-    /**
-     * Bootstrap the application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $this->publishes([
-            realpath(__DIR__ . '/../../config/appmanager.php'), config_path('appmanager.php')
-        ]);
-    }
 
     /**
      * Register the application services.
@@ -37,8 +21,16 @@ class AppManagerServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->singleton('manage.source', function ($app) {
-            return new SourceManager();
-        });
+
+    }
+    
+        /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        
     }
 }
